060bfc73842481a52621831a580d26354dc4c80b
Nayshlok <nineborland@gmail.com>
2014-11-24 15:41:38 -0700

    Erased fileupload code from get controller and deleted account controller test

--- Voyager/src/Controllers/GetController.java
+++ Voyager/src/Controllers/GetController.java
@@ -20,60 +20,6 @@ import models.ModelAndView;
 
 public class GetController {
 	
-	protected static String processRequest(HttpServletRequest request,
-	        HttpServletResponse response, String filePath)
-	        throws ServletException, IOException {
-	    response.setContentType("text/html;charset=UTF-8");
-
-	    // Create path components to save the file
-	    final String path = filePath + "\\resources\\images";
-	    System.out.println(filePath);
-	    final Part filePart = request.getPart("image");
-	    final String fileName = getFileName(filePart);
-
-	    OutputStream out = null;
-	    InputStream filecontent = null;
-	    final PrintWriter writer = response.getWriter();
-
-	    try {
-	        out = new FileOutputStream(new File(path + File.separator
-	                + fileName));
-	        filecontent = filePart.getInputStream();
-
-	        int read = 0;
-	        final byte[] bytes = new byte[1024];
-
-	        while ((read = filecontent.read(bytes)) != -1) {
-	            out.write(bytes, 0, read);
-	        }
-	        writer.println("New file " + fileName + " created at " + path);
-
-	    } catch (FileNotFoundException fne) {
-	        writer.println("You either did not specify a file to upload or are "
-	                + "trying to upload a file to a protected or nonexistent "
-	                + "location.");
-	        writer.println("<br/> ERROR: " + fne.getMessage());
-
-	    }finally{
-	    	if(out != null){
-	    		out.close();
-	    	}
-	    }
-
-	    return fileName;
-	}
-
-	public static String getFileName(final Part part) {
-	    final String partHeader = part.getHeader("content-disposition");
-	    for (String content : part.getHeader("content-disposition").split(";")) {
-	        if (content.trim().startsWith("filename")) {
-	            return content.substring(
-	                    content.indexOf('=') + 1).trim().replace("\"", "");
-	        }
-	    }
-	    return null;
-	}
-		
 	public ModelAndView getUserList(HttpServletRequest request, DataService dataService){
 		List<Account> allUsers = dataService.getAllUsers();
 		request.setAttribute("userList", allUsers);
