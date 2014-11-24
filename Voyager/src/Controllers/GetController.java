fa62b25dbc97ae70a3e3159480431aed99e11479
nkagee <nkagee@student.neumont.edu>
2014-11-24 15:37:30 -0700

    Started consolidating get requests into single class; getProfile and getSingleLocation methods still need to be filled in.

---
+++ Voyager/src/Controllers/GetController.java
@@ -0,0 +1,106 @@
+package Controllers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import services.DataService;
+import models.Account;
+import models.ModelAndView;
+
+public class GetController {
+	
+	protected static String processRequest(HttpServletRequest request,
+	        HttpServletResponse response, String filePath)
+	        throws ServletException, IOException {
+	    response.setContentType("text/html;charset=UTF-8");
+
+	    // Create path components to save the file
+	    final String path = filePath + "\\resources\\images";
+	    System.out.println(filePath);
+	    final Part filePart = request.getPart("image");
+	    final String fileName = getFileName(filePart);
+
+	    OutputStream out = null;
+	    InputStream filecontent = null;
+	    final PrintWriter writer = response.getWriter();
+
+	    try {
+	        out = new FileOutputStream(new File(path + File.separator
+	                + fileName));
+	        filecontent = filePart.getInputStream();
+
+	        int read = 0;
+	        final byte[] bytes = new byte[1024];
+
+	        while ((read = filecontent.read(bytes)) != -1) {
+	            out.write(bytes, 0, read);
+	        }
+	        writer.println("New file " + fileName + " created at " + path);
+
+	    } catch (FileNotFoundException fne) {
+	        writer.println("You either did not specify a file to upload or are "
+	                + "trying to upload a file to a protected or nonexistent "
+	                + "location.");
+	        writer.println("<br/> ERROR: " + fne.getMessage());
+
+	    }finally{
+	    	if(out != null){
+	    		out.close();
+	    	}
+	    }
+
+	    return fileName;
+	}
+
+	public static String getFileName(final Part part) {
+	    final String partHeader = part.getHeader("content-disposition");
+	    for (String content : part.getHeader("content-disposition").split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
+	}
+		
+	public ModelAndView getUserList(HttpServletRequest request, DataService dataService){
+		List<Account> allUsers = dataService.getAllUsers();
+		request.setAttribute("userList", allUsers);
+		return new ModelAndView(allUsers, "/WEB-INF/Users.jsp");
+	}
+	
+	public ModelAndView beginLoginWorkflow() {
+		return new ModelAndView(null, "/WEB-INF/account/login.jsp");
+	}
+	
+	public ModelAndView beginRegisterWorkflow() {
+		return new ModelAndView(null, "/WEB-INF/account/register.jsp");
+	}
+	
+	public ModelAndView getHomePage() {
+		return new ModelAndView(null, "index.jsp");
+	}
+	
+	public ModelAndView getProfilePage() {
+		return null;
+	}
+	
+	public ModelAndView beginLocationSubmissionWorkflow() {
+		return new ModelAndView(null, "submit.jsp");
+	}
+	
+	public ModelAndView getSingleLocation() {
+		return null;
+	}
+}
